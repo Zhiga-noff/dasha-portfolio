a47e36248118b2ae0a4bc29acad4e31a40669c6a
KZHiganov <kzhiganov@zvezdamedia.ru>
2024-09-10 16:57:58 +0300

    Сделал анимацию плавного скролла в начало сайта при переходе на другую страницу

--- src/app/pages/home/components/company/company.component.ts
+++ src/app/pages/home/components/company/company.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { ICompany } from '../../../../../libs/types/company.types';
+import { DOCUMENT, ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'z-company',
@@ -9,4 +10,13 @@ import { ICompany } from '../../../../../libs/types/company.types';
 export class CompanyComponent {
   @Input() item: ICompany;
   @Input() back: boolean;
+
+  constructor(
+    private viewScroller: ViewportScroller,
+    @Inject(DOCUMENT) private document: Document,
+  ) {}
+
+  clickToBtn() {
+    this.viewScroller.scrollToPosition([0, 0]);
+  }
 }
