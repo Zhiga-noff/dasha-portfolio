e896b0f443ab81396d2120979b320459d9c3faae
KZHiganov <kzhiganov@zvezdamedia.ru>
2024-09-10 13:35:30 +0300

    Поправил отображение активного роута

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,19 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { menu } from '../../../../libs/constant/memu.constant';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'z-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   readonly menu = menu;
-  protected activeRoute: string = 'Проекты';
+  protected activeRoute: string;
   protected activeDrop: boolean = false;
+
+  private routeSubscription: Subscription;
+
+  constructor(protected router: Router) {}
+
+  ngOnInit() {
+    this.routeSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = event.urlAfterRedirects;
+        if (currentRoute === '/') {
+          this.activeRoute = 'Проекты';
+        }
+
+        this.menu.forEach((item) => {
+          if (item.link === currentRoute) {
+            this.activeRoute = item.name;
+          }
+        });
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
 
   setActiveRoute(nameRoute: string) {
     this.activeRoute = nameRoute;
     this.activeDrop = false;
+    this.activeDrop = !this.activeDrop;
   }
 
   callDropMenu() {
